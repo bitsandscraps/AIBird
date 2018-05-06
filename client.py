890e96684ba6203f5b1f0c6684d0cba6af63322e
PARK Jongeui <jongeui.park@kaist.ac.kr>
2018-05-06 23:36:09 +0900

    add timeout to socket. add docstrings

--- client.py
+++ client.py
@@ -1,9 +1,25 @@
+"""AIBird Client module"""
 import socket
 import message
 
-MAX_TRIALS = 5
+TIMEOUT = 5         # Timeout value for the socket
 
 class AIBirdClient:
+    """Handles communication with the AIBird Server
+
+    Attributes
+        host (str): address of AIBird Server. Default is `localhost`
+        port (int): port of AIBird Server. Default is `2004`
+        team_id(int): team ID. Default is `1`
+
+    Usage:
+        client = AIBirdClient()
+        client.connect()
+        # Do whatever you want to do.
+        img = client.screenshot()
+        shoot = process_screenshot(img)     # A user defined function
+        client.polar_shoot(*shoot)
+    """
     def __init__(self, host='localhost', port=2004, team_id=1):
         self.host = host
         self.port = port
@@ -11,61 +27,101 @@ class AIBirdClient:
         self.socket = None
 
     def connect(self):
+        """Connect to AIBird server."""
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(TIMEOUT)
         self.socket.connect((self.host, self.port))
         self.socket.sendall(message.configure(self.team_id))
         data = self.socket.recv(message.LEN_CONFIGURE)
         message.recv_configure(data)
 
     def screenshot(self):
+        """Get screenshot
+
+        Return a numpy array containing the screenshot"""
         self.socket.sendall(message.get_screenshot())
-        size = message.recv_screenshot_size(self.socket.recv(message.LEN_SCREENSHOT))
+        size = message.recv_screenshot_size(
+                self.socket.recv(message.LEN_SCREENSHOT))
         return message.recv_screenshot(self.socket.recv(size))
 
-    def best_score(self):
-        self.socket.sendall(message.get_best_score())
-        return message.recv_score(self.socket.recv(message.LEN_GET_SCORE))
-
     def my_score(self):
+        """Get my score
+
+        Return a list of lenth 21 where each slot indicates a best score of
+        the corresponding level.
+        """
         self.socket.sendall(message.get_my_score())
         return message.recv_score(self.socket.recv(message.LEN_GET_SCORE))
 
     def current_level(self):
+        """Get current level"""
         self.socket.sendall(message.get_current_level())
-        return message.recv_current_level(self.socket.recv(message.LEN_GET_CURRENT_LEVEL))
+        return message.recv_current_level(
+                self.socket.recv(message.LEN_GET_CURRENT_LEVEL))
 
-    def retry_until_success(self, msg):
-        for _ in range(MAX_TRIALS):
-            self.socket.sendall(msg)
-            if message.recv_result(self.socket.recv(message.LEN_ETC)):
-                return True
-        return False
+    def send_and_recv_result(self, msg):
+        """Send msg and parse its result.
+
+        Return True if succeeded, False otherwise.
+        """
+        self.socket.sendall(msg)
+        return message.recv_result(self.socket.recv(message.LEN_ETC))
 
     def cart_shoot(self, fx, fy, dx, dy, t1, t2, mode='safe'):
-        if not self.retry_until_success(message.cart_shoot(fx, fy, dx, dy, t1, t2, mode)):
-            raise ValueError('Max Trial Exceeded: cart_shoot')
-        
+        """Send cart_shoot request.
+
+        Args
+            fx, fy -- x, y coordinate of slingshot
+            dx, dy -- relative x, y coordinate of release point w.r.t (fx,fy)
+            t1 -- release time
+            t2 -- gap between release time and tap time
+
+        Return True if accepted, False if rejected.
+        """
+        return self.send_and_recv_result(
+                message.cart_shoot(fx, fy, dx, dy, t1, t2, mode))
+
     def polar_shoot(self, fx, fy, theta, r, t1, t2, mode='safe'):
-        if not self.retry_until_success(message.polar_shoot(fx, fy, theta, r, t1, t2, mode)):
-            raise ValueError('Max Trial Exceeded: polar_shoot')
-        
+        """Send cart_shoot request.
+
+        Args
+            fx, fy -- x, y coordinate of slingshot
+            theta, r -- relative polar coordinates of release point w.r.t (fx,fy)
+            t1 -- release time
+            t2 -- gap between release time and tap time
+
+        Return True if accepted, False if rejected.
+        """
+        return self.send_and_recv_result(
+                message.polar_shoot(fx, fy, theta, r, t1, t2, mode))
+
     def zoom_in(self):
-        if not self.retry_until_success(message.zoom_in()):
-            raise ValueError('Max Trial Exceeded: zoom_in')
+        """Send zoom in request.
+        Return True if succeeded, False otherwise.
+        """
+        return self.send_and_recv_result(message.zoom_in())
 
     def zoom_out(self):
-        if not self.retry_until_success(message.zoom_out()):
-            raise ValueError('Max Trial Exceeded: zoom_out')
+        """Send zoom out request.
+        Return True if succeeded, False otherwise.
+        """
+        return self.send_and_recv_result(message.zoom_out())
 
     def click_in_center(self):
-        if not self.retry_until_success(message.click_in_center()):
-            raise ValueError('Max Trial Exceeded: click_in_center')
+        """Send click in center request.
+        Return True if succeeded, False otherwise.
+        """
+        return self.send_and_recv_result(message.click_in_center())
 
     def load_level(self, level):
-        if not self.retry_until_success(message.load_level(level)):
-            raise ValueError('Max Trial Exceeded: load_level')
+        """Send load level `level` request.
+        Return True if succeeded, False otherwise.
+        """
+        return self.send_and_recv_result(message.load_level(level))
 
     def restart_level(self):
-        if not self.retry_until_success(message.restart_level()):
-            raise ValueError('Max Trial Exceeded: restart_level')
+        """Send restart level request.
+        Return True if succeeded, False otherwise.
+        """
+        return self.send_and_recv_result(message.restart_level())
 
--- message.py
+++ message.py
@@ -1,5 +1,16 @@
-import struct
+"""Module that handles AIBird server client communication protocol
 
+Example:
+    send(get_screenshot())          # Sends a screenshot request message
+    result = recv(LEN_SCREENSHOT)   # Receives the response
+    size, width, height = recv_screenshot_size(result)
+    screenshot_raw = recv(size)
+    screenshot = recv_screenshot(screenshot_raw, width, height)
+"""
+import struct
+import numpy as np
+
+# Message IDs
 MID_CONFIGURE = 1
 MID_SCREENSHOT = 11
 MID_GET_STATE = 12
@@ -18,13 +29,15 @@ MID_CLICK_IN_CENTER = 36
 MID_LOAD_LEVEL = 51
 MID_RESTART_LEVEL = 52
 
-LEN_CONFIGURE = 3               # Round Info = 1, Time Limit = 1, Available Levels = 1
+# Length of response messages
+LEN_CONFIGURE = 3       # Round Info = 1, Time Limit = 1, Available Levels = 1
 LEN_SCREENSHOT = 4 + 4          # Width = 4, Height = 4
 LEN_GET_STATE = 1
-LEN_GET_SCORE = 21 * 4       # Score for each Level = 4, 21 Levels
+LEN_GET_SCORE = 21 * 4          # Score for each Level = 4, 21 Levels
 LEN_GET_CURRENT_LEVEL = 1       # Current Level = 1
 LEN_ETC = 1                     # OK/ERR
 
+# Response of a current state request message
 STATE_UNKNOWN = 0
 STATE_MAIN_MENU = 1
 STATE_EPISODE_MENU = 2
@@ -35,56 +48,93 @@ STATE_WON = 6
 STATE_LOST = 7
 
 def configure(team_id):
+    """Formulate a configure message"""
     return struct.pack('!bi', MID_CONFIGURE, team_id)
 
 def recv_configure(result):
+    """Parse the response of a configure message"""
     round_info, time_limit, available_levels = struct.unpack('!bbb', result)
     if round_info == 0:
         raise ValueError('Configuration Failed')
     return available_levels
 
 def get_screenshot():
+    """Formulate a screenshot request message"""
     return struct.pack('!b', MID_SCREENSHOT)
 
 def recv_screenshot_size(result):
-    width, height = struct.unpack('!ii', result)
-    print('width = {}, height = {}'.format(width, height))
-    return width * height * 3           # with * height RGB tuples
+    """Parse the received size information.
 
-def recv_screenshot(result):
+    Arguments
+    result -- received data
+
+    returns the tuple (image data size in bytes, width, height)
+    """
+    width, height = struct.unpack('!ii', result)
+    return width * height * 3, width, height        # with * height RGB tuples
+
+def recv_screenshot(result, width, height):
+    """Parse the stream into an image
+
+    Arguments
+    result -- received data
+    width -- width of the image
+    height -- height of the image
+
+    returns a numpy array of (height, width, 3)
+    """
     screenshot = []
     for red, green, blue in struct.iter_unpack('!bbb', result):
         screenshot.append([red, green, blue])
-    return screenshot
+    np_screenshot = np.ndarray(screenshot, dtype=np.uint8)
+    return np_screenshot.resize((height, width, 3))
 
 def get_state():
+    """Formulate a message requesting the current state"""
     return struct.pack('!b', MID_GET_STATE)
 
 def recv_state(result):
+    """Parse the response of a state request message"""
     return struct.unpack('!b', result)
 
 def get_best_score():
+    """Formulate a message requesting the best score"""
     return struct.pack('!b', MID_GET_BEST_SCORE)
 
 def get_my_score():
+    """Formulate a message requesting my score"""
     return struct.pack('!b', MID_GET_MY_SCORE)
 
 def recv_score(result):
+    """Parse a response of a score request message.
+
+    Returns a list of length 21, where i-th element is the i-th level score.
+    """
     score_list = []
     for score in struct.iter_unpack('!b', result):
         score_list.append(score)
     return score_list
 
 def get_current_level():
+    """Formulate a message requesting the current level information"""
     return struct.pack('!b', MID_GET_CURRENT_LEVEL)
 
 def recv_current_level(result):
+    """Parse a response of a current level request message
+
+    Returns an integer representing the current level.
+    """
     level = struct.unpack('!b', result)
     if level < 0 or level > 21:
         raise ValueError('Received current level = ' + level)
     return level
 
 def cart_shoot(fx, fy, dx, dy, t1, t2, mode='safe'):
+    """Formulate a Cartesian shoot request message.
+
+    Keyword Arg
+    mode -- 'safe' and 'fast'
+    """
     if mode == 'safe':
         mid = MID_CART_SHOOT_SAFE
     elif mode == 'fast':
@@ -94,6 +144,11 @@ def cart_shoot(fx, fy, dx, dy, t1, t2, mode='safe'):
     return struct.pack('!biiiiii', mid, fx, fy, dx, dy, t1, t2)
 
 def polar_shoot(fx, fy, theta, r, t1, t2, mode='safe'):
+    """Formulate a polar shoot request message.
+
+    Keyword Arg
+    mode -- 'safe' and 'fast'
+    """
     if mode == 'safe':
         mid = MID_POLAR_SHOOT_SAFE
     elif mode == 'fast':
@@ -103,20 +158,42 @@ def polar_shoot(fx, fy, theta, r, t1, t2, mode='safe'):
     return struct.pack('!biiiiii', mid, fx, fy, theta, r, t1, t2)
 
 def zoom_out():
+    """Formulate a zoom out request message"""
     return struct.pack('!b', MID_FULL_ZOOM_OUT)
 
 def zoom_in():
+    """Formulate a zoom in request message"""
     return struct.pack('!b', MID_FULL_ZOOM_IN)
 
 def click_in_center():
+    """Formulate a click in center request message"""
     return struct.pack('!b', MID_CLICK_IN_CENTER)
 
 def load_level(level):
+    """Formulate a load level request message.
+
+    level -- integer representing the level to load
+    """
     return struct.pack('!bb', MID_LOAD_LEVEL, level)
 
 def restart_level():
+    """Formulate a restart level request message"""
     return struct.pack('!b', MID_RESTART_LEVEL)
 
 def recv_result(result):
-    return struct.unpack('!b', result)
+    """Parse a response of the following request messages
+
+    Available request messages:
+    cart_shoot, polar_shoot, zoom_out, zoom_in, click_in_center,
+    load_level, restart_level
+
+    Return True when succeeded, False otherwise
+    """
+    res = struct.unpack('!b', result)
+    if res == 1:
+        return True
+    elif res == 0:
+        return False
+    else:
+        raise ValueError('recv_result received: ' + res)
 
